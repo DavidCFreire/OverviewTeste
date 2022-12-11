dcab69c768b4c89f8a9cac191453d7b0a92ac190
viniciusraony <viniciusraony@hotmail.com>
2022-12-11 16:56:12 -0300

    - ADD CRUD

--- OverviewTeste/Form1.Designer.cs
+++ OverviewTeste/Form1.Designer.cs
@@ -28,21 +28,29 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.listView1 = new System.Windows.Forms.ListView();
             this.label1 = new System.Windows.Forms.Label();
             this.txtNome = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.dtpNascimento = new System.Windows.Forms.DateTimePicker();
             this.btnAdicionar = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.updateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label3 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // listView1
             // 
             this.listView1.Location = new System.Drawing.Point(12, 58);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(776, 341);
+            this.listView1.Size = new System.Drawing.Size(776, 321);
             this.listView1.TabIndex = 0;
             this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseClick);
             // 
             // label1
             // 
@@ -86,12 +94,48 @@
             this.btnAdicionar.TabIndex = 5;
             this.btnAdicionar.Text = "Adicionar";
             this.btnAdicionar.UseVisualStyleBackColor = true;
+            this.btnAdicionar.Click += new System.EventHandler(this.btnAdicionar_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.deleteToolStripMenuItem,
+            this.updateToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(122, 48);
+            // 
+            // deleteToolStripMenuItem
+            // 
+            this.deleteToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("deleteToolStripMenuItem.Image")));
+            this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(121, 22);
+            this.deleteToolStripMenuItem.Text = "Remover";
+            this.deleteToolStripMenuItem.Click += new System.EventHandler(this.deleteToolStripMenuItem_Click);
+            // 
+            // updateToolStripMenuItem
+            // 
+            this.updateToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("updateToolStripMenuItem.Image")));
+            this.updateToolStripMenuItem.Name = "updateToolStripMenuItem";
+            this.updateToolStripMenuItem.Size = new System.Drawing.Size(121, 22);
+            this.updateToolStripMenuItem.Text = "Atualizar";
+            this.updateToolStripMenuItem.Click += new System.EventHandler(this.updateToolStripMenuItem_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Black", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(12, 385);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(202, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "QUANTIDADE REGISTROS:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 411);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.btnAdicionar);
             this.Controls.Add(this.dtpNascimento);
             this.Controls.Add(this.label2);
@@ -100,6 +144,8 @@
             this.Controls.Add(this.listView1);
             this.Name = "Form1";
             this.Text = "Cadastro Usuarios";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -113,5 +159,9 @@
         private Label label2;
         private DateTimePicker dtpNascimento;
         private Button btnAdicionar;
+        private ContextMenuStrip contextMenuStrip1;
+        private ToolStripMenuItem deleteToolStripMenuItem;
+        private ToolStripMenuItem updateToolStripMenuItem;
+        private Label label3;
     }
 }
--- OverviewTeste/Form1.cs
+++ OverviewTeste/Form1.cs
@@ -6,5 +6,67 @@ namespace OverviewTeste
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        { 
+            // SETAR PROPRIEDAES listView
+            listView1.View = View.Details;
+            listView1.FullRowSelect = true;
+            listView1.GridLines = true;
+            listView1.LabelEdit = true;
+
+            listView1.Columns.Add("Nome", 650);
+            listView1.Columns.Add("Data Nasc", 116);
+            
+            // SETAR CURSOR PARA INICIAR NO TEXTBOX DO LABEL NOME
+            txtNome.Select();          
+        }
+
+        // CONTADOR DE REGISTRO
+        int countreg = 0;
+        private void contador(int countreg)
+        {
+            label3.Text = "QUANTIDADE DE REGISTRO: ";
+            label3.Text = label3.Text + ' ' + countreg;
+        }
+                
+        // METÓDO BOTÃO ADICIONAR
+        private void btnAdicionar_Click(object sender, EventArgs e)
+        {
+            // Validar Nome para não ser vazio (trim retirar espaços antes e depois e comparar se é vazia)
+            if (txtNome.Text.Trim().Equals(string.Empty))
+            {
+                MessageBox.Show("O nome não você ser vazio", Text, MessageBoxButtons.OK, MessageBoxIcon.Error); // Título do formulário
+                return;
+            }
+
+            ListViewItem item = new ListViewItem();
+            item.SubItems.Add(txtNome.Text.Trim());
+            item.SubItems.Add(dtpNascimento.Text);
+            countreg += 1;
+            contador(countreg);
+            listView1.Items.Add(item);
+        }
+        // MÉTODO RIGHT CLICK NA LISTVIEW - ContextMenuStrip
+        private void listView1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+                contextMenuStrip1.Show(Cursor.Position);
+        }
+        // MÉTODO BOTÃO REMOVER - ContextMenuStrip 
+        private void deleteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            countreg -= 1;
+            contador(countreg);
+            listView1.Items.RemoveAt(listView1.SelectedIndices[0]);
+            
+        }
+        // MÉTODO BOTÃO ATUALIZAR - ContextMenuStrip
+        private void updateToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listView1.SelectedItems[0].SubItems[0].Text = txtNome.Text;
+            listView1.SelectedItems[0].SubItems[1].Text = dtpNascimento.Text;
+        }
+      
     }
 }
